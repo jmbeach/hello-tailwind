5945b246975215fa3fef9828caa5663463485233
jmbeach <jaredbeachdesign@gmail.com>
2020-12-19 23:12:21 -0600

    explain wrapper

--- 1_chitchat/src/App.js
+++ 1_chitchat/src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
 
 function App() {
+  /**
+   * Explained:
+   *  p-6: "padding 6" on all sides of the div https://tailwindcss.com/docs/padding
+   *  max-w-sm: sets the max-width of the div to 24rem https://tailwindcss.com/docs/max-width
+   *  mx-auto: sets the margin to auto in the horizontal direction https://tailwindcss.com/docs/margin
+   *  bg-white: background white (duh) https://tailwindcss.com/docs/background-color
+   *  rounded-xl: uses large rounding on the corners - 0.75rem https://tailwindcss.com/docs/border-radius
+   *  flex: makes the div display = flex https://tailwindcss.com/docs/flex
+   *  items-center: flex modifier to set align-items = center https://tailwindcss.com/docs/align-items
+   *  space-x-4: modifies space between elements in x direction https://tailwindcss.com/docs/space#class-reference
+   */
   return (
     <div className="App">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
